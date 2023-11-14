fc5cfc1be7a253767365a16ef3c6e436d4113658
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2023-11-13 17:29:37 -0800

    Worked on Player Controls
    Created an input action C# script for the jumping movement and created a jumping function in player controller.

--- SuperPetrJumper/Assets/Scripts/PlayerController.cs
+++ SuperPetrJumper/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody2D rbPlayer;
+    [SerializeField] private float jumpForce;
+    private PetrControllers playerControls;
+
+    private void Awake()
     {
-        
+        playerControls = new PetrControllers();
+        playerControls.Petr.Enable();
+        playerControls.Petr.Jump.performed += Jump;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rbPlayer.AddForce(Vector2.up * jumpForce);
+        }
     }
 }
