c7f6226f6adf5768222298e9ca58c38c87d9ee9e
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2023-11-13 21:48:38 -0800

    Gojo Superpower
    Created a scriptable object for Gojo Super power and tested a power up object. Started working on a power-up spawner.

---
+++ SuperPetrJumper/Assets/Scripts/GojoOverPowerUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PowerUpsScriptable/GojoOverPowerUp")]
+
+public class GojoOverPowerUp : PowerUpsScriptable
+{
+    [SerializeField] private Sprite GojoSkin;
+
+    public override void ChangeSkins (GameObject target)
+    {
+        target.GetComponentInChildren<SpriteRenderer>().sprite = GojoSkin;
+    }
+}
---
+++ SuperPetrJumper/Assets/Scripts/PowerUpsScriptable.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUpsScriptable : ScriptableObject
+{
+    public abstract void ChangeSkins(GameObject target);
+}
